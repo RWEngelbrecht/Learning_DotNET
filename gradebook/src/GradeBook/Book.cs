4c5da3b0b6f83d9026668b2e1d7f0df6390baec8
rigardt.engelbrecht <rigardt.engelbrecht@global.ntt>
2021-05-21 11:19:05 +0200

    moved statistics logic to book class

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook {
   class Book {
 
     List<double> grades;
+    
     public Book() {
       grades = new List<double>();
     }
@@ -17,6 +19,21 @@ namespace GradeBook {
 
     public List<double> GetGrades() {
       return new List<double>();
+    }
+
+    public void ShowStatistics() {
+      var avg = 0.0;
+      var highest = double.MinValue;
+      var lowest = double.MaxValue;
+      var median = 50.0;
+      foreach(var grade in grades) {
+        highest = Math.Max(grade, highest);
+        lowest = Math.Min(grade, lowest);
+        avg += grade;
+      }
+      median = grades[grades.Count / 2];
+      avg /= grades.Count;
+      Console.WriteLine($"Average: {avg:N1}\nHighest: {highest}\nLowest: {lowest}\nMedian: {median}");
     }
 
   }
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -11,17 +11,8 @@ namespace GradeBook
       Book book = new Book();
       book.AddGrade(89.1);
       book.AddGrade(90.5);
-
-      List<double> grades = new List<double>();
-
-      grades.Add(56.1);
-
-      var avg = 0.0;
-      foreach(var grade in grades) {
-        avg += grade;
-      }
-      avg /= grades.Count;
-      Console.WriteLine($"Average is: {avg:N1}");
+      book.AddGrade(77.7);
+      book.ShowStatistics();
     }
   }
 }
